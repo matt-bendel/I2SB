59fc77af0aa07d0393e46ba89f56164f0a2669fc
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2025-02-17 01:43:16 -0500

    Added PSNR log

--- i2sb/runner_hybrid.py
+++ i2sb/runner_hybrid.py
@@ -175,7 +175,7 @@ class Runner(object):
                 for l, step_int in enumerate(step_list):
                     if step_int <= N_unroll:
                         step[l] = N_unroll + 1
-                        unroll_steps[l, :] = np.arange(N_unroll + 1)
+                        unroll_steps[l, :] = torch.tensor(np.arange(N_unroll + 1)).to(x0.device)
                     else:
                         idx = np.round(np.linspace(0, step_int - 1, N_unroll)).astype(int)
                         unroll_steps[l, :] = torch.tensor(np.arange(step_int)[idx]).to(x0.device)
