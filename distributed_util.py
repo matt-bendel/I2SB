0d67108438466ecabbf0c8ba50fa37d1da2770d4
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2025-02-20 23:33:38 -0500

    LPIPS/PSNR test script

--- distributed_util.py
+++ distributed_util.py
@@ -16,7 +16,7 @@ from torch.multiprocessing import Process
 def init_processes(rank, size, fn, args):
     """ Initialize the distributed environment. """
     os.environ['MASTER_ADDR'] = args.master_address
-    os.environ['MASTER_PORT'] = '6020'
+    os.environ['MASTER_PORT'] = '6021'
     torch.cuda.set_device(args.local_rank)
     dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=size)
     fn(args)
